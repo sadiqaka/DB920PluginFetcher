28663183f241689bc1afc0953abe48775f42aaf6
sadiqaka <42109899+sadiqaka@users.noreply.github.com>
2025-06-09 17:31:32 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+RSS_FEEDS = [
+    "https://example.com/feed"
+]
+YOUTUBE_FEEDS = [
+    "https://www.youtube.com/feeds/videos.xml?channel_id=YOUR_CHANNEL_ID"
+]
+WORDPRESS_SITES = [
+    {
+        "url": "https://yourwp/xmlrpc.php",
+        "username": "youruser",
+        "password": "yourpass"
+    }
+]
